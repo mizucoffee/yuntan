06569013d0bd3bd79afff6828d5bef9463ec0694
Ritsuki Goto <develop@mizucoffee.net>
2019-01-03 21:00:06 +0900

    [add] tslint & prettier

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,42 +1,44 @@
-import { app, BrowserWindow, App } from 'electron'
+import { app, App, BrowserWindow } from 'electron'
 import __basedir from '../basedir'
 
 class Yuntan {
-    private mainWindow: BrowserWindow | null = null
-    private app: App
-    private mainUrl: string = `file://${__basedir}/src/index.html`
+  private mainWindow: BrowserWindow | null = null
+  private app: App
+  private mainUrl: string = `file://${__basedir}/src/index.html`
 
-    constructor(app: App) {
-        this.app = app
-        this.app.on('window-all-closed',() => this.onWindowAllClosed())
-        this.app.on('ready',() => this.onReady())
-        this.app.on('activate',() => this.onActivated())
+  constructor(app: App) {
+    this.app = app
+    this.app.on('window-all-closed', () => this.onWindowAllClosed())
+    this.app.on('ready', () => this.onReady())
+    this.app.on('activate', () => this.onActivated())
+  }
+
+  private create() {
+    this.mainWindow = new BrowserWindow({
+      height: 720,
+      width: 1280
+    })
+
+    this.mainWindow.loadURL(this.mainUrl)
+
+    this.mainWindow.on('closed', () => {
+      this.mainWindow = null
+    })
+  }
+
+  private onWindowAllClosed() {
+    this.app.quit()
+  }
+
+  private onReady() {
+    this.create()
+  }
+
+  private onActivated() {
+    if (this.mainWindow === null) {
+      this.create()
     }
-
-    private create() {
-        this.mainWindow = new BrowserWindow({
-            width: 1280,
-            height: 720,
-        })
-
-        this.mainWindow.loadURL(this.mainUrl)
-
-        this.mainWindow.on('closed', () => {
-            this.mainWindow = null
-        })
-    }
-
-    private onWindowAllClosed() {
-        this.app.quit()
-    }
-
-    private onReady() {
-        this.create()
-    }
-
-    private onActivated() {
-        if(this.mainWindow === null) this.create()
-    }
+  }
 }
 
 new Yuntan(app)
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,54 @@
-const path = require('path')
-const merge = require('webpack-merge')
+const path = require("path");
+const merge = require("webpack-merge");
 
 const common = {
   node: {
     __dirname: false,
-    __filename: false,
+    __filename: false
   },
-  mode: process.env.ENV || 'development',
+  mode: process.env.ENV || "development",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: [/node_modules/],
+        use: "ts-loader",
+        exclude: [/node_modules/]
+      },
+      {
+        enforce: "pre",
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "tslint-loader",
+            options: {
+              typeCheck: true,
+              fix: true
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"]
   }
-}
+};
 
 module.exports = [
   merge(common, {
-    target: 'electron-main',
-    entry: './src/main/index.ts',
+    target: "electron-main",
+    entry: "./src/main/index.ts",
     output: {
-      path: path.resolve(__dirname, 'dist/main'),
-      filename: 'main.js',
+      path: path.resolve(__dirname, "dist/main"),
+      filename: "main.js"
     }
   }),
   merge(common, {
-    target: 'electron-renderer',
-    entry: './src/renderer/index.ts',
+    target: "electron-renderer",
+    entry: "./src/renderer/index.ts",
     output: {
-      path: path.resolve(__dirname, 'dist/renderer'),
-      filename: 'main.js',
+      path: path.resolve(__dirname, "dist/renderer"),
+      filename: "main.js"
     }
   })
-]
+];
